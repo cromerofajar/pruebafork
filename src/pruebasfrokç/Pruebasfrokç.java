bbdfd8aad8da033bc53b6c426ebfd632641ee771
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-11-27 09:48:49 +0100

    primero

---
+++ src/pruebasfrokç/Pruebasfrokç.java
@@ -0,0 +1,14 @@
+
+package pruebasfrokç;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Pruebasfrokç {
+
+    public static void main(String[] args) {
+        // los comentarios van al final del archivo    
+    }
+    
+}
